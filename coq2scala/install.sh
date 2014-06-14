3e884ac3939fb97e34bac15f46188dad0126c5a5
amutake <amutake.s@gmail.com>
2014-06-14 22:42:49 +0900

    Fix coq2scala installation

--- coq2scala/install.sh
+++ coq2scala/install.sh
@@ -4,7 +4,6 @@ apt-get update
 apt-get upgrade -y
 apt-get install -y build-essential curl m4 git
 curl -kL https://raw.github.com/hcarty/ocamlbrew/master/ocamlbrew-install | env OCAMLBREW_BASE="/opt/ocamlbrew" OCAMLBREW_FLAGS="-o -n ocaml" bash
-cat /tmp/ocamlbrew*
 export PATH=/opt/ocamlbrew/ocaml/bin:$PATH
 git clone git://github.com/hemmi/coq2scala.git /opt/coq2scala
 cd /opt/coq2scala/coq-*
